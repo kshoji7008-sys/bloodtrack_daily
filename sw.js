9eb0d52d32075677f35ef58d1c2b752481f11223
kshoji7008-sys <k.shoji7008@gmail.com>
2025-10-08 13:48:03 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request).catch(function() {
-    return caches.match(event.request);
-  }));
-});
